1fa8d98f4a3e22a27057ad59e9308059d3def03f
upieez <samuelhww@gmail.com>
2019-12-20 15:21:23 +0800

    add function to check if the donut box is the same amount as the one in the donut array

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ console.log("Donut game project!")
 var count = 0;
 
 
-//----Add Donut Function ----
+//----Input Donut Function to Donut Box ----
 var addDonut = function () {
+//If statement to only add donuts if it's less than 6
     if (count < 6){
         count++;
-        var donutBox = document.querySelector(".donut-box");
+        var donutBox = document.querySelector(".donut-takeaway");
         var div = document.createElement("div");
         div.classList.add("donut");
         div.innerHTML = "DONUT PLACEMENT";
@@ -22,3 +23,28 @@ clicker.addEventListener("click", addDonut);
 };
 
 addDonutToContainer();
+
+//----Check for correct amount of donut in donut box from donut array----
+
+var checkForCorrectAmt = function(){
+
+var donutBox = document.querySelector(".donut-takeaway").innerText;
+var donutArray = document.querySelector(".donut-array").innerText;
+var checkDonutArray = donutArray.split("\n");
+var checkDonutBox = donutBox.split("\n");
+//----If statement to check the correct amount in box----
+ if (checkDonutArray.length === checkDonutBox.length){
+    alert("correct!");
+    } else {
+        alert("wrong!");
+    }
+};
+
+var buttonChecker = function(){
+var selectButton = document.querySelector("button");
+selectButton.addEventListener("click", checkForCorrectAmt);
+};
+
+buttonChecker();
+
+//----
