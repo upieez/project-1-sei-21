4b55e6e1ddc7a7d10b2f56ac97cf6420a78da2c1
upieez <samuelhww@gmail.com>
2019-12-20 09:31:43 +0800

    add index html, script js and style css file

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Donut game project!")
+
+var container = document.querySelector(".container");
+var words = document.createElement("div");
+words.textContent = "TEST 1 PHASE IN PROGRESS!";
+container.appendChild(words);
